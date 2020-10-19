5962246f2eedd21eedccb8e8eb93f7fa0967eefa
Dominik <dominikbutz@gmail.com>
2020-10-19 13:59:55 +0800

    Initial Commit

---
+++ Sources/SwipeCellSUI/SwipeCellSUI.swift
@@ -0,0 +1,3 @@
+struct SwipeCellSUI {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import SwipeCellSUITests
+
+var tests = [XCTestCaseEntry]()
+tests += SwipeCellSUITests.allTests()
+XCTMain(tests)
